4d9931ca26f662476e5794b733febc33621f7e40
Vladimir_Kirsanovgit config --global user.email Vladimir_Kirsanov <Vladimir_Kirsanov@epam.com>
2021-04-05 11:44:16 +0300

    add Parameters for Android SDK, and Appium

---
+++ src/FirstTest.java
@@ -0,0 +1,37 @@
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.net.URL;
+
+public class FirstTest {
+
+
+    private AndroidDriver<?> driver;
+
+    @Before
+    public void SetUp() throws Exception {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "AndroidTestDevice");
+        capabilities.setCapability("platformVersion", "8.0");
+        capabilities.setCapability("automationName", "Appium");
+        capabilities.setCapability("appPackage", "org.wikipedia");
+        capabilities.setCapability("appActivity", ".main.MainActivity");
+        capabilities.setCapability("app", new File("./apks/org.wikipedia.apk").getCanonicalPath());
+        driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+
+    @After
+    public void TearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void firstTest() {
+        System.out.println("First test run");
+    }
+}
